724cadadcfb0af90fa041b2fe3c68709fb9d8869
dom thomas <domtom1126@gmail.com>
2022-08-11 22:04:52 -0400

    Added thoughts route and run with ngrok for linode

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
 @app.route('/')
 def hello_world():
@@ -9,3 +11,11 @@ def hello_world():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/thoughts')
+def thoughts():
+    return render_template('thoughts.html')
+
+
+if __name__ == '__main__':
+    app.run()
